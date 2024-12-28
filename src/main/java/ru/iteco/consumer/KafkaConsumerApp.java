8d1ca1673d9743638864573317f8724e53807649
Mihail <t9034049980@gmail.com>
2024-12-28 16:34:25 +0300

    Перенос классов по функциональным папкам

---
+++ src/main/java/ru/iteco/consumer/KafkaConsumerApp.java
@@ -0,0 +1,18 @@
+package ru.iteco.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.util.Properties;
+
+public class KafkaConsumerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Consumer!");
+
+
+
+    }
+
+}
--- src/main/java/ru/iteco/producer/KafkaProducerTransactionalApp.java
+++ src/main/java/ru/iteco/producer/KafkaProducerTransactionalApp.java
@@ -1,4 +1,4 @@
-package ru.iteco;
+package ru.iteco.producer;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
