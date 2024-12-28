5619f64017ea925ab6163a71df33907b659df025
Mihail <t9034049980@gmail.com>
2024-12-28 14:41:37 +0300

    Initial commit

---
+++ src/main/java/ru/iteco/KafkaConsumerApp.java
@@ -0,0 +1,12 @@
+package ru.iteco;
+
+public class KafkaConsumerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Consumer!");
+
+
+
+    }
+
+}
---
+++ src/main/java/ru/iteco/KafkaProducerApp.java
@@ -0,0 +1,30 @@
+package ru.iteco;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.header.internals.RecordHeader;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class KafkaProducerApp {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(KafkaProducerApp.class);
+
+    public static void main(String[] args) {
+        System.out.println("Hello Producer!");
+        var properties = new Properties();
+        properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092, localhost:39092, localhost:49092");
+        properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+
+
+
+    }
+}
